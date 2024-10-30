a2c00f50f64e2db6f4f84e239f5c516a800e06e3
maxirpark <maxirutipark@gmail.com>
2024-10-30 10:24:51 +0900

    lenis wheelMultiplier: 0.35,

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -8,7 +8,19 @@ const Providers: React.FC<Props> = ({ children }) => {
   return (
     // options={{ syncTouch: true }}
     <>
-      <ReactLenis root>{children}</ReactLenis>
+      <ReactLenis
+        root
+        options={{
+          lerp: 0.09,
+          // smoothWheel: true,
+          wheelMultiplier: 0.35, // Higher multiplier for more pronounced scroll
+          // touchMultiplier: 2, // Higher multiplier for touch events
+          // syncTouchLerp: 0.1, // Adjust syncTouch lerp
+          // touchInertiaMultiplier: 50, // Increase inertia multiplier
+        }}
+      >
+        {children}{" "}
+      </ReactLenis>
     </>
   );
 };
